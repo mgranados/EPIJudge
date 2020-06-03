c6c42d47eaee7b1c0593bf8a80c66c15c26da84f
Martín Granados García <martin.granados@wizeline.com>
2020-06-03 00:49:32 -0500

    Add dutch national flag

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -9,8 +9,44 @@ RED, WHITE, BLUE = range(3)
 
 
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    # O(n) time
+    # O(1) time
+    B = list(A)
+    pivot = A[pivot_index]
+    bottom = 0
+    middle = 0
+    bigger = len(A) - 1
+
+    while middle <= bigger:
+        if A[middle] < pivot:
+            A[bottom], A[middle] = A[middle], A[bottom]
+            bottom += 1
+            middle +=1
+        elif A[middle] == pivot:
+            middle += 1
+        elif A[middle] > pivot:
+            A[bigger], A[middle] = A[middle], A[bigger]
+            bigger -= 1
+
+    return B
+
+    # O(n) time
+    # O(1) space
+    # one pass, send to beg < index and to len -1 > index
+    #pivot = A[pivot_index]
+    #smaller = 0
+    #for i in range(len(A)):
+    #    if A[i] < pivot:
+    #        A[i], A[smaller] = A[smaller], A[i]
+    #        smaller += 1
+    #bigger = len(A) - 1
+    #for i in reversed(range(len(A))):
+    #    if A[i] < pivot: 
+    #        break
+    #    elif A[i] > pivot:
+    #        A[i], A[bigger] = A[bigger], A[i]
+    #        bigger -= 1
+    #return A
 
 
 @enable_executor_hook
--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,10 +1,24 @@
 from test_framework import generic_test
-
+import math
 
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # 2 ^ 4 * 2 ^ 3 = 2 ^ 7
+    # (2 ^ m) ^ n = 2 ^ (m * n)
+    result = 1
+    power_2 = math.floor(abs(y/2))
+    odd = y % 2 == 1
+    for i in range(power_2):
+        result *= x * x
 
+    if odd:
+        result *= x
+
+    if y < 0:   
+        return 1 / result
+ 
+    return result 
+
+# at worst 2* biggest power of 2 ~ O(n)
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
