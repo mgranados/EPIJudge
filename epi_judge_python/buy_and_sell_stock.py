e77f044e6eecff793937b9d4828f8ee550de9a18
Martín Granados García <martin.granados@wizeline.com>
2020-06-03 22:54:54 -0500

    Solved Buy and Sell stock

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -4,8 +4,32 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    # O(n) 
+    # set begin
+    begin = prices[0]
+    max_diff = 0
+
+    for i in range(1, len(prices)):
+        if begin > prices[i]:
+            begin = prices[i] 
+        else:
+            # max(prices[i] - begin , max_diff)
+            if prices[i] - begin > max_diff:
+                max_diff = prices[i] - begin
+
+    # Variant
+    #subarray_length = 1
+    #max_sub = 0
+    #for i in range(1, len(prices)):
+    #    if begin != prices[i]:
+            #max_sub = max(max_sub, subarray_length)
+            #subarray_length = 1
+    #        begin = prices[i]
+    #    else:
+    #        subarray_length += 1
+    #return max_sub
+
+    return max_diff 
 
 
 if __name__ == '__main__':
