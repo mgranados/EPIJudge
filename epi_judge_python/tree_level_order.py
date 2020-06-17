e151c64618504746cffa453b11a0dd09ad6bc498
Martín Granados García <martin.granados@wizeline.com>
2020-06-16 23:22:13 -0500

    add tree level order queue

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -2,11 +2,37 @@ from typing import List
 
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
-
+from collections import deque
 
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    # 2 queues
+    parents = deque()
+    children = deque()
+    result =  []
+    
+    # push node to parents
+    if tree:
+        parents.append(tree)
+
+    # pop from parents until done, push to result
+    parentList = []
+    while parents: # popleft will return something
+        #pop and get children
+        rooty = parents.popleft()
+        if rooty.left:
+            children.append(rooty.left)
+        if rooty.right:
+            children.append(rooty.right)
+
+        parentList.append(rooty.data)
+
+        if not parents: # reviewed all parents 
+            result.append(parentList)
+            parentList = []
+            # invert lists
+            children, parents = parents, children
+
+    return result
 
 
 if __name__ == '__main__':
