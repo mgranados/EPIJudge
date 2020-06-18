63d48d5605d335b1782c76849e6abe8a1ce188e0
Martín Granados García <martin.granados@wizeline.com>
2020-06-18 00:27:45 -0500

    Add tree height balanced

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -1,11 +1,23 @@
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
-
+import math
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+        
+    def getDepth(node):
+        if not node:
+            return 0
+        if abs(getDepth(node.left) - getDepth(node.right)) > 1:
+            raise Exception('depth was too big')
+        
+        return 1 + max(getDepth(node.left), getDepth(node.right))
 
+    try:
+        depth = getDepth(tree) >= 0
+    except:
+        return False
+
+    return depth
 
 if __name__ == '__main__':
     exit(
