cc6962a8ae125f23b7ee4a6163d5b0ba0e406334
Martin Granados Garcia <margrangr@gmail.com>
2020-06-26 18:30:58 -0500

    Add search first divide

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,9 +4,21 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    low = 0
+    up = len(A) - 1
+    while low <= up:
+        med = low + (up - low) // 2
+        if A[med] < k:
+            low = med + 1
+        elif A[med] == k:
+            if med > 0 and A[med - 1] == k:
+                up = med - 1
+            else:
+                return med
+        else:
+            up = med - 1
+        # not seen
+    return -1
 
 if __name__ == '__main__':
     exit(
