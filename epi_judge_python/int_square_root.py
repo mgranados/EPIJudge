110d9b894a795cc59a65aed9349f16a79650103e
Martin Granados Garcia <margrangr@gmail.com>
2020-06-26 21:23:45 -0500

    Add int_square_root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def square_root(k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    if k == 0:
+        return k
+
+    low = 1
+    end = k
+    while low <= end:
+        mid = low + (end - low) // 2
+        if mid * mid == k: #we done
+            return mid
+        elif mid * mid > k: # let's keep going bin
+            end = mid
+        elif mid * mid < k: # let's keep going
+            n_mid = mid +1
+            if (n_mid * n_mid) > k: # but if next is > k we done
+                return mid
+            else: 
+                low = mid
+
+    return -1
 
 
 if __name__ == '__main__':
