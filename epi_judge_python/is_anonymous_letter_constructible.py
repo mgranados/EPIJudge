2d765a1f16994f66ba0588f4def81b663c3e66e8
Martin Granados Garcia <margrangr@gmail.com>
2020-07-01 22:32:10 -0500

    Add anonymous letter

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,9 +1,26 @@
 from test_framework import generic_test
+import collections
 
 
 def is_letter_constructible_from_magazine(letter_text: str,
                                           magazine_text: str) -> bool:
-    # TODO - you fill in here.
+    # tiempo lineal = O(n) + O(m)
+    mag_count = {}
+    for char in magazine_text:
+        if char in mag_count:
+            mag_count[char] += 1
+        else:
+            mag_count[char] = 1
+
+    for char in letter_text:
+        if char not in mag_count:
+            return False
+        if char in mag_count:
+            mag_count[char] -= 1
+
+        if mag_count[char] < 0:
+            return False
+
     return True
 
 
