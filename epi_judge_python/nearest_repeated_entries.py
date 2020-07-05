5bb745a93d02710871d817b8eda07e4fe1a3443f
Martín Granados García <martin.granados@wizeline.com>
2020-07-04 22:01:53 -0500

    Add nearest repeated entries hash

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,11 +1,21 @@
 from typing import List
 
 from test_framework import generic_test
-
+import math
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # traverse O(n)
+    # space O(d) distinct entries
+    last_idx = {}
+    min_dist = math.inf
+    for idx, word in enumerate(paragraph):
+        if word in last_idx:
+            if idx - last_idx[word] < min_dist:
+                min_dist = idx - last_idx[word] 
+        last_idx[word] = idx
+        
+
+    return min_dist if min_dist != math.inf else -1
 
 
 if __name__ == '__main__':
