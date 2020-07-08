0e48fca23c7cbe9711bb1a04b6cf9196fb936734
Martín Granados García <martin.granados@wizeline.com>
2020-07-07 23:54:28 -0500

    Add intersect sorted arrays

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -4,8 +4,20 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    idx_a, idx_b = 0, 0
+    intersections = []
+    while idx_a < len(A) and idx_b < len(B):
+        if A[idx_a] == B[idx_b]:
+            if not intersections or intersections[-1] != A[idx_a]:
+                intersections.append(A[idx_a])
+            idx_a += 1
+            idx_b += 1
+        elif A[idx_a] > B[idx_b]:
+            idx_b += 1
+        elif A[idx_a] < B[idx_b]:
+            idx_a += 1
+
+    return intersections
 
 
 if __name__ == '__main__':
