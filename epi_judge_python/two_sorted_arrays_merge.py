7860e9c489d4a30fcc20069bcaa44949366543b6
Martín Granados García <martin.granados@wizeline.com>
2020-07-08 23:22:31 -0500

    Add two sorted arrays

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -5,8 +5,28 @@ from test_framework import generic_test
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
-    return
+    #final of A is m + n -1
+    end_a = m + n - 1
+    idx_a = m - 1
+    idx_b = n - 1
+    # iterate and compare a and b 
+    while idx_a >= 0 or idx_b >= 0:
+        if idx_a < 0:
+            A[end_a] = B[idx_b]
+            idx_b  -= 1
+        elif idx_b < 0:
+            A[end_a] = A[idx_a]
+            idx_a -= 1
+        elif B[idx_b] > A[idx_a]:
+            A[end_a] = B[idx_b]
+            idx_b -= 1
+        else:
+            A[end_a] = A[idx_a]
+            idx_a -= 1
+        end_a -= 1
+
+        
+    return A
 
 
 def merge_two_sorted_arrays_wrapper(A, m, B, n):
