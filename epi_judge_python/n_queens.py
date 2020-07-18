e7e59ee4fc01c1bfb70aa710f53d57c4b976c35e
Martín Granados García <martin.granados@wizeline.com>
2020-07-17 22:49:46 -0500

    Add n queens midly solved

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,31 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    def solver(row):
+        if row == n:
+            result.append(placement.copy())
+            return
+        for column in range(n):
+            all_pass = []
+            for i, c in enumerate(placement[:row]):
+                # i = row
+                # c = value of column
+                all_pass.append(
+                    abs(c - column) != 0 and # same col
+                    abs(c-column) != row - i) # a diagonal
+
+            if all(all_pass):
+                placement[row] = column
+                solver(row+1)
+
+
+    placement = [0] * n
+    solver(0)
+        
+    return result
+
+
 
 
 def comp(a, b):
