7e126777d303c2b33eb62af40461fff74457f410
Martín Granados García <martin.granados@wizeline.com>
2020-07-21 22:47:56 -0500

    Add greedy prob 7.4

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -2,10 +2,18 @@ from typing import List
 
 from test_framework import generic_test
 
-
 def has_three_sum(A: List[int], t: int) -> bool:
-    # TODO - you fill in here.
-    return True
+    def two_sum(B, s):
+        seted = set(B)
+        for n in B:
+            if s - n in seted:
+                return True
+        return False
+
+    for num in A:
+        if two_sum(A, t - num):
+            return True
+    return False
 
 
 if __name__ == '__main__':
