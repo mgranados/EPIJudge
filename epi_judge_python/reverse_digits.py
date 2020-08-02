45bb78931b6e150443e1f10cdc2cc47df25483e7
Martin Granados Garcia <margrangr@gmail.com>
2020-08-01 21:53:07 -0500

    Add Reverse Digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -1,9 +1,21 @@
 from test_framework import generic_test
-
+import math
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    sign = -1 if x < 0 else 1
+    result_pow = 0
+    result = 0
+    abs_x = abs(x)
+    power = math.floor(math.log10(abs_x))
+
+    while power >= 0:
+        current = math.floor(abs_x / (10 ** power))
+        result += current * (10 ** result_pow)
+        abs_x -= current * 10 ** power
+        power -= 1
+        result_pow += 1
+
+    return result * sign
 
 
 if __name__ == '__main__':
