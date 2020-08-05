6f58134b1d2e8d7e6d0e0c4eb03a217bdaf06cf5
Martín Granados García <martin.granados@wizeline.com>
2020-08-04 22:00:38 -0500

    Add is palindromic punctuation

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -2,7 +2,23 @@ from test_framework import generic_test
 
 
 def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    # test from [i, , , , j]
+    i = 0
+    j = len(s) - 1
+    while j > i:
+        if not s[i].isalnum():
+            i += 1
+            continue
+        if not s[j].isalnum():
+            j -= 1
+            continue
+
+        if s[i].lower() != s[j].lower():
+            return False
+
+        i+=1
+        j-=1
+
     return True
 
 
