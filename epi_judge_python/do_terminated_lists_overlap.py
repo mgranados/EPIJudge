1818b0c96a6957f724296757b701767a307ef188
Martín Granados García <martin.granados@wizeline.com>
2020-08-06 22:13:14 -0500

    Add list and reversed words

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -7,8 +7,42 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+    if not l0 or not l1:
+        return None
+
+    def list_len(list_to_test):
+        count = 0
+        while(list_to_test.next):
+            count += 1
+            list_to_test = list_to_test.next
+        return count
+
+    len0 = list_len(l0)
+    len1 = list_len(l1)
+
+    #save only diff
+
+    if len0 > len1: 
+        while len0 > len1 and l0.next:
+            l0 = l0.next
+            len0 -= 1
+        
+    elif len1 > len0:
+        while len1 > len0 and l1.next:
+            l1 = l1.next
+            len1 -= 1
+        
+    while len1 > 0:
+        if not l0 or not l1:
+            return None
+
+        if l0 == l1:
+            return l0
+        l0 = l0.next
+        l1 = l1.next
+        len1 -= 1
+
+    return None
 
 
 @enable_executor_hook
