dcc2689ed6b5bb41bf116eaa441fb37684d762c8
Martín Granados García <martin.granados@wizeline.com>
2020-08-10 22:18:56 -0500

    Add tree symmetry

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,8 +3,20 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+
+    def help_simmetry(t0, t1):
+        if not t0 and not t1:
+            return True
+
+        if (t0 and not t1) or (t1 and not t0) or (t0.data != t1.data):
+            return False
+
+        return help_simmetry(t0.left, t1.right) and help_simmetry(t0.right, t1.left)
+
+    if not tree:
+        return True
+    # only root
+    return help_simmetry(tree.left, tree.right)
 
 
 if __name__ == '__main__':
