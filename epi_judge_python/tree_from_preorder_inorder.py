b8af460506f65532889931769dff2e9a54debba2
Martín Granados García <martin.granados@wizeline.com>
2020-08-12 23:20:26 -0500

    Add naive version

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -6,8 +6,30 @@ from test_framework import generic_test
 
 def binary_tree_from_preorder_inorder(preorder: List[int],
                                       inorder: List[int]) -> BinaryTreeNode:
-    # TODO - you fill in here.
-    return BinaryTreeNode()
+    def helper(root, preorder):
+        node = BinaryTreeNode(root)
+
+        pivot = inorder.index(root)
+        left = inorder[:pivot]
+        right = inorder[pivot+1:]
+        left_nodes = []
+        right_nodes = []
+        for val in preorder:
+            if val in left:
+                left_nodes.append(val)
+            else:
+                right_nodes.append(val)
+
+        if left_nodes:
+            node.left = helper(left_nodes[0], left_nodes[1:] )
+        if right_nodes:
+            node.right = helper(right_nodes[0], right_nodes[1:] )
+        return node
+
+    if not preorder or not inorder:
+        return None
+
+    return helper(preorder[0], preorder[1:])
 
 
 if __name__ == '__main__':
