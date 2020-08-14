0de4e0d3b3577cc20bbc465723f6046286b788b5
Martín Granados García <martin.granados@wizeline.com>
2020-08-13 22:08:45 -0500

    Add sort_almost_sorted_array.py

--- epi_judge_python/sort_almost_sorted_array.py
+++ epi_judge_python/sort_almost_sorted_array.py
@@ -1,12 +1,24 @@
 from typing import Iterator, List
 
 from test_framework import generic_test
-
+import heapq
 
 def sort_approximately_sorted_array(sequence: Iterator[int],
                                     k: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    k_heap = []
+
+    seq = iter(sequence)
+
+    for i in range(k):
+        heapq.heappush(k_heap, next(seq))
+
+    for val in seq:
+        result.append(heapq.heappushpop(k_heap, val))
+
+    result.extend(heapq.nsmallest(k, k_heap))
+
+    return result
 
 
 def sort_approximately_sorted_array_wrapper(sequence, k):
