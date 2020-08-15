b511527d9d6ec7292ea864b6c0d513a673dd2915
Martín Granados García <martin.granados@wizeline.com>
2020-08-15 01:01:55 -0500

    Add search shifted sorted array, changed mid taking into account the cycle 449, 190, 200

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def search_smallest(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    # binary search
+    # if mid > right 
+    # min must be there because a cycle was done
+    # else
+    # 546, 103, 442, 455, 466, 477
+    right = len(A) - 1
+    left = 0
+
+    while left < right:
+        mid = (left + right) // 2
+        if A[mid] > A[right]:
+            left = mid + 1
+        else:
+            right = mid
+
+
+    return right
 
 
 if __name__ == '__main__':
