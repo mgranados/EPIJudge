26365c11c7d9c6cbe74e2fa4b21e843782cc101c
Martín Granados García <martin.granados@wizeline.com>
2020-08-25 10:32:49 -0500

    Added permutations book take

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -4,8 +4,23 @@ from test_framework import generic_test, test_utils
 
 
 def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    result = []
+    if len(A) <= 1:
+        return [A]
+
+    def recursive_helper(index):
+        if index == len(A) - 1: # we at the last one
+            result.append(A.copy())
+            return
+
+        for i in range(index, len(A)): # [1, 2, 3]
+            A[index], A[i] = A[i], A[index] # [2, 1, 3]
+            recursive_helper(index + 1) # [2, *1*, 3]
+            A[i], A[index] = A[index], A[i] # [1, 2, 3]
+
+    recursive_helper(0)
+
+    return result
 
 
 if __name__ == '__main__':
