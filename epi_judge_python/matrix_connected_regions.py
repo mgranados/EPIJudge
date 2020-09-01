3d638c38fb50393f03a9eba412455d3db9da8e90
Martín Granados García <martin.granados@wizeline.com>
2020-09-01 09:03:24 -0500

    Add matrix connected regions
    The problem here could arbitrarily move onto the next ones via recursion with the consideration if those where the same color, then flipping, for the other (BFS) you needed a queue and push and pop because you where just checking once again if the color was different

--- epi_judge_python/matrix_connected_regions.py
+++ epi_judge_python/matrix_connected_regions.py
@@ -4,8 +4,24 @@ from test_framework import generic_test
 
 
 def flip_color(x: int, y: int, image: List[List[bool]]) -> None:
-    # TODO - you fill in here.
-    return
+    # BFS => visit all adjacent and check if color needs switch
+    color = not image[x][y]
+
+    def flip_helper(x, y, image):
+        if image[x][y] != color:
+            image[x][y] = color
+            if x < len(image) - 1:
+                flip_helper(x + 1, y, image)
+            if x > 0:
+                flip_helper(x - 1, y, image)
+            if y < len(image) - 1:
+                flip_helper(x, y + 1, image)
+            if y > 0:
+                flip_helper(x, y - 1, image)
+
+    flip_helper(x, y, image)
+
+    return image
 
 
 def flip_color_wrapper(x, y, image):
