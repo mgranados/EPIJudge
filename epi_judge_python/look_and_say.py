12fda7e262c0bceb0c6bcef4d7d12ab32cada04f
Martín Granados García <martin.granados@wizeline.com>
2020-09-05 00:28:00 -0500

    Add look and say

--- epi_judge_python/look_and_say.py
+++ epi_judge_python/look_and_say.py
@@ -2,8 +2,33 @@ from test_framework import generic_test
 
 
 def look_and_say(n: int) -> str:
-    # TODO - you fill in here.
-    return ''
+    if not n:
+        return ''
+
+    if n == 1:
+        return '1'
+
+    result = "1"
+    for _ in range(1, n):
+        last_number = None
+        reps = 0
+        local = []
+        for char in result:
+            # "describe" it
+            if not last_number:
+                last_number = char
+                reps = 1
+            elif char == last_number:
+                reps += 1
+            else: # reset
+                local.append(str(reps) + last_number)
+                last_number = char
+                reps = 1
+        local.append(str(reps) + last_number)
+        result = "".join(local)
+
+
+    return result
 
 
 if __name__ == '__main__':
