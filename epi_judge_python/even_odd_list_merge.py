dfea4ccdbf6df8961d6bff214f1cdc5753b4b94c
Martín Granados García <martin.granados@wizeline.com>
2020-09-06 10:54:51 -0500

    Add Even odd list merge

--- epi_judge_python/even_odd_list_merge.py
+++ epi_judge_python/even_odd_list_merge.py
@@ -5,8 +5,29 @@ from test_framework import generic_test
 
 
 def even_odd_merge(L: ListNode) -> Optional[ListNode]:
-    # TODO - you fill in here.
-    return None
+    if L is None:
+        return L
+    odd = ListNode()
+    even = ListNode()
+    head_odd = odd
+    head_even = even
+    odd_count = False
+    while L:
+        # if odd send to other list
+        if odd_count:
+            odd.next = L
+            odd = odd.next
+        else:
+            even.next = L
+            even = even.next
+        odd_count = not odd_count
+        L = L.next
+
+    odd.next = None
+    even.next = None
+
+    even.next = head_odd.next
+    return head_even.next
 
 
 if __name__ == '__main__':
