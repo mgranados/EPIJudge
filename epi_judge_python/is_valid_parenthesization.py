1208f8d776f6bed323eb51d9984acacc5a8e8688
Martín Granados García <martin.granados@wizeline.com>
2020-09-07 09:38:18 -0500

    Add valid parenthesis

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,29 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    stack = []
+    if len(s) == 1:
+        return False
+    for char in s:
+        if char == '}':
+            if not stack or stack[-1] != '{':
+                return False
+            else:
+                stack.pop()
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                return False
+            else:
+                stack.pop()
+        elif char == ']':
+            if not stack or stack[-1] != '[':
+                return False
+            else:
+                stack.pop()
+        else:
+            stack.append(char)
+
+    return not stack
 
 
 if __name__ == '__main__':
