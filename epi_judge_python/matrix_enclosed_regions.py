1d4dc79fda98b43804c1e7f9a765b9d9b61bd89c
Martin Granados Garcia <margrangr@gmail.com>
2020-10-08 22:08:40 -0500

    Add matrix of enclosing white ones

--- epi_judge_python/matrix_enclosed_regions.py
+++ epi_judge_python/matrix_enclosed_regions.py
@@ -1,12 +1,33 @@
 from typing import List
 
 from test_framework import generic_test
-
+import collections
 
 def fill_surrounded_regions(board: List[List[str]]) -> None:
-    # TODO - you fill in here.
-    return
+    m = len(board)
+    n = len(board[0])
+    q = collections.deque()
 
+    for k in range(m):
+        for i, j in ((k,0), (k, n - 1)):
+            q.append((i, j))
+
+    for k in range(n):
+        for i, j in ((0,k), (m - 1, k)):
+            q.append((i, j))
+
+    while q:
+        x, y = q.popleft()
+        if 0 <= x < m and 0 <= y < n and board[x][y] == 'W':
+            board[x][y] = 'R'
+            q.extend([(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)])
+
+    for x in range(m):
+        for y in range(n):
+            if board[x][y] != 'R':
+                board[x][y] = 'B'
+            else:
+                board[x][y] = 'W'
 
 def fill_surrounded_regions_wrapper(board):
     fill_surrounded_regions(board)
