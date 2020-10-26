5620d2cdda8cf807ea532062da6fb2237c45bda7
Martin Granados Garcia <margrangr@gmail.com>
2020-10-25 21:24:54 -0600

    Add October 25

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -1,11 +1,34 @@
 from typing import List
 
 from test_framework import generic_test
+import bintrees
+import math
 
+class Number:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.val = a + b * math.sqrt(2)
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __eq__(self, other):
+        return self.val == other.val
 
 def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
-    # TODO - you fill in here.
-    return []
+    # Initial 
+    candidates = bintrees.RBTree([(Number(0,0), None)])
+
+    result = []
+    while len(result) < k:
+        next_smallest = candidates.pop_min()[0]
+        result.append(next_smallest.val)
+
+        candidates[Number(next_smallest.a + 1, next_smallest.b)] = None
+        candidates[Number(next_smallest.a, next_smallest.b + 1)] = None
+
+    return result
 
 
 if __name__ == '__main__':
